1fe2a805cb0cee57c49e0c165fdf7b4bdefadcac
tamtam <ibrahimtamtama17@gmail.com>
2017-10-24 23:49:00 +0700

    nomor 4 ngasih fungsi faktorial, udah bener tapi belom urut

--- soal4.c
+++ soal4.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <string.h>
+#include <pthread.h>
+
+void *faktorial(void *arg)
+{
+    int bil = (int *) arg;
+    int hasil=1,i;
+    for(i=1;i<=bil;i++){
+        hasil *= i;
+    }
+    printf("Hasil %d! = %d\n", bil, hasil);
+}
 
 int main(int argc, char** argv)
 {
-    int inp,flag=1,arr[100],j,i,angka,pow;
+    int inp,j,i,angka,pow;
+    pthread_t tid[argc];
     for(j=1;j<argc;j++) {
         i=0;
         int len=strlen(argv[j]);
@@ -17,6 +29,9 @@ int main(int argc, char** argv)
             inp += angka;
         }
         inp += argv[j][strlen(argv[j])-1]-48;
-        printf("\n%d\n", inp);
+        pthread_create(&(tid[j]), NULL, &faktorial, (void*)inp);
     }
+
+    for(i=0;i<argc;i++)
+        pthread_join(tid[i], NULL);
 }
