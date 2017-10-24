550c970aef7fdc55a81d19aee27cfa091ab1f794
tamtam <ibrahimtamtama17@gmail.com>
2017-10-25 03:49:42 +0700

    nomor 5 udah dibikin threadnya, udah selesai nomor 5

--- soal5.c
+++ soal5.c
@@ -1,34 +1,42 @@
 #include <stdio.h>
 #include <string.h>
+#include <pthread.h>
 
 FILE *fp;
 
-int main(int argc, char** argv)
+void *cari(void* arg)
 {
     fp = fopen("Novel.txt", "r");
-    int i;
 
     size_t pos = ftell(fp);
     fseek(fp, 0, SEEK_END);
     size_t len = ftell(fp);
     fseek(fp, pos, SEEK_SET);
 
-    char text[len];
-    fgets (text, len, fp);
+    char text[len+1];
+    fgets (text, len+1, fp);
+    
+    char *key = (char *)arg;
+    int key_len = strlen(key);
+    int count = 0;
+
+    char *where = text;
+    if (key_len)
+        while ((where = strstr(where, key))) {
+            where += key_len;
+            count++;
+        }
+    printf("%s : %d\n", key, count);
+    fclose(fp);
+}
+
+int main(int argc, char** argv)
+{
+    int i;
+    pthread_t tid[argc-1];
 
     for(i=1;i<argc;i++){
-        char *key = argv[i];
-
-        int key_len = strlen(key);
-        int count = 0;
-
-        char *where = text;
-        if (key_len)
-            while ((where = strstr(where, key))) {
-                where += key_len;
-                count++;
-            }
-        printf("%s : %d\n", key, count);
+        pthread_create(&(tid[i-1]), NULL, &cari, (void*)argv[i]);
+        pthread_join(tid[i-1], NULL);
     }
-    fclose(fp);
 }
