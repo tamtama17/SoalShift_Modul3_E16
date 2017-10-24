97d17608021e874314ac49e8386cb334f02de7f1
tamtam <ibrahimtamtama17@gmail.com>
2017-10-25 03:32:13 +0700

    nomor 5 udah bisa cuma belom pake thread

--- soal5.c
+++ soal5.c
@@ -1,31 +1,34 @@
 #include <stdio.h>
 #include <string.h>
 
-int main()
+FILE *fp;
+
+int main(int argc, char** argv)
 {
-    FILE *novel = fopen("Novel.txt", "r");
+    fp = fopen("Novel.txt", "r");
+    int i;
 
-    size_t pos = ftell(novel);
-    fseek(novel, 0, SEEK_END);
-    size_t len = ftell(novel);
-    fseek(novel, pos, SEEK_SET);
+    size_t pos = ftell(fp);
+    fseek(fp, 0, SEEK_END);
+    size_t len = ftell(fp);
+    fseek(fp, pos, SEEK_SET);
 
-    char text[len+1];
-    fgets (text, len+1, novel);
-    printf("%s\n\n", text);
+    char text[len];
+    fgets (text, len, fp);
 
-    char *key = "Ifah";
+    for(i=1;i<argc;i++){
+        char *key = argv[i];
 
-    int key_len = strlen(key);
-    int count = 0;
+        int key_len = strlen(key);
+        int count = 0;
 
-    char *where = text;
-
-    if (key_len)
-        while ((where = strstr(where, key))) {
-            where += key_len;
-            count++;
-        }
-    printf("%d\n", count);
-    fclose(novel);
+        char *where = text;
+        if (key_len)
+            while ((where = strstr(where, key))) {
+                where += key_len;
+                count++;
+            }
+        printf("%s : %d\n", key, count);
+    }
+    fclose(fp);
 }
